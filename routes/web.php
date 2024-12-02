559bb26559a29ce57e5184b0b3211dce6bc8071a
Davidbmor <dbarmor1901@ieszaidinvergeles.org>
2024-12-02 23:39:45 +0000

    Arreglo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
+// routes/web.php
 
+use App\Http\Controllers\ArchivoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArchivoController::class, 'index']);
+Route::get('archivos/table', [ArchivoController::class, 'table'])->name('archivos.table');
+Route::resource('archivos', ArchivoController::class);
+Route::get('archivos/{id}', [ArchivoController::class, 'show'])->name('archivos.show');
